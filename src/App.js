2e3013e0a537babcdae6df19009c3edc9822ff45
dthrasher90 <dthrasher90@GMAIL.COM>
2020-08-16 13:24:25 -0700

    test

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <h4> Test push  </h4>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
